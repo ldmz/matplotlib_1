789f9e7fea8568ae27127070e4f51ef046ca699c
ldmz <ldmz.dev@gmail.com>
2023-01-20 13:28:21 +0200

    plot visual correction

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import matplotlib.pyplot as plt
 squares = [1, 4, 9, 16, 25, 36]
 
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(squares, linewidth=3)
+
+# names and fonsize
+ax.set_title("Square numbers", fontsize=14)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square value", fontsize=14)
+
+# labels fontsize
+ax.tick_params(axis='both', labelsize=14)
 
 plt.show()
